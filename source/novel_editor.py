b21d6dabea9c7f855004d83be1e3f337e1f75f3f
Yoshihiro Yamahara <yoshihiro@yamahara.email>
2019-09-22 23:25:23 +0900

    コメントを追加

--- source/novel_editor.py
+++ source/novel_editor.py
@@ -100,6 +100,7 @@ class LineFrame(ttk.Frame):
         self.text.bind('<Control-Key-e>', self.open_file)
         # 保存ダイアロクを表示する
         self.text.bind('<Control-Key-w>', self.save_file)
+        # 小説家になろうを開く
         self.text.bind('<Control-Key-u>', self.open_url)
         # 上書き保存する
         self.text.bind('<Control-Key-s>', self.overwrite_save_file)
