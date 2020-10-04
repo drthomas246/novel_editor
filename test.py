80e4e6084d82b532057c4b37f0bdd24af631183b
Dr?Thomas <drthomas@hobofoto.net>
2020-10-04 14:29:39 +0900

    test.pyの変更

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-with open('.coverage') as f:
-    print(f.read())
+import os
+
+
+print(os.path.realpath(__file__))
+print(os.path.abspath(__file__))
+
+# with open('.coverage') as f:
+#     print(f.read())
