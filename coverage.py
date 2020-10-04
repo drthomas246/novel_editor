3763bc196a309694cf938918e8abefe9dbbb90fd
Dr?Thomas <drthomas@hobofoto.net>
2020-10-04 16:05:29 +0900

    coverage.pyの変更

--- coverage.py
+++ coverage.py
@@ -1,9 +1,5 @@
 with open('.coverage') as f:
     t = f.read()
-    s = t.replace(
-        'C:\\\\Users\\\\drtho\\\\github\\\\novel_editor\\\\source\\\\',
-        "/home/scrutinizer/build/source/"
-    )
 
 with open('.coverage', mode='w') as f:
-    f.write(s)
+    f.write(t)
