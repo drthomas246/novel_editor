06b135aaf70ed204f851027313a1f6cf07182dc9
Dr?Thomas <drthomas@hobofoto.net>
2020-10-04 15:25:54 +0900

    test.pyの変更

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-with open('.coverage', mode='w') as f:
+with open('.coverage') as f:
     t = f.read()
     s = t.replace(
         'C:\\\\Users\\\\drtho\\\\github\\\\novel_editor\\\\source\\\\',
         "/home/scrutinizer/build/source/"
     )
+
+with open('.coverage', mode='w') as f:
     f.write(s)
