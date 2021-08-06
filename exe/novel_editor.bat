282ac6cc29798ab98609aeee4cc0fb6d29ae4311
drthomas246 <yoshihiro@yamahara.email>
2021-08-07 08:48:00 +0900

    exeファイル作成方法を変更

---
+++ exe/novel_editor.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCqDJFqL8EcMJhRCSQi9OW+zEroO1O3j6taCpkQJW/BxcYzUug==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJgZkIaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsEAl3MbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIeJw1RQESnOGKuCad8
+::eg0/rx1wNQPfEVWB+kM9LVsJDAqNKm62ObAY4fz0/aSCukh9
+::fBEirQZwNQPfEVWB+kM9LVsJDCqNKm62RpAY4fz0/Yo=
+::cRolqwZ3JBvQF1fEqQKzyPfRnOsK9qc/yGOfCSN4DiAEQKCetQS8mGJYO3oNwiiQCaJnl1WuNKU6
+::dhA7uBVwLU+EWGCF+UM4KQlVDD2NL2OzDrwO54g=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE100gMQldSwyWfMlzRudMuri71uuKo0UcRuN/QYDI0r2GKfIcig==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJEmW+0UiKRZoQguUOWeFA7EV/Ofp0+SC7EgFUYI=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+.\dist\python.exe .\dist\novel_editor.py
